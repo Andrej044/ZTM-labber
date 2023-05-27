16afc6ff76400a89774afbabbdf8e13e6bd84f92
Andrii Lishchyna <andrej044@i.ua>
2023-05-26 23:27:34 -0400

    add second task - must be used map method

--- jsPractics/script.js
+++ jsPractics/script.js
@@ -61,3 +61,5 @@ const newArray = [];
 array.forEach(item => {
   newArray.push(item.username = item.username + "!")
 })
+
+//Create an array using map that has all the usernames with a "? to each of the usernames
