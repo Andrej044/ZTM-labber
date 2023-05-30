c98dbf71bb3a10483061bec2dfd510d3602a2b5c
Andrii Lishchyna <andrej044@i.ua>
2023-05-30 14:37:58 -0400

    add basic setup's for cleaned up function, sorted array from less to max

--- jsPractics/script.js
+++ jsPractics/script.js
@@ -144,3 +144,14 @@ function checkBasket(basket, lookingFor) {
 // make a function that organizes these into individual array that is ordered.
 // For example answer(ArrayFromAbove) should return: [[1,1,1,1],[2,2,2], 4,5,10,[20,20], 391, 392,591].
 // Bonus: Make it so it organizes strings differently from number types. i.e. [1, "2", "3", 2] should return [[1,2], ["2", "3"]]
+
+const messArray = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
+
+const cleanArrayFunc = (arr) => {
+  const cleanedArray = arr.sort((a,b) => a-b).map( item => { 
+    
+  });
+  return cleanedArray
+}
+
+cleanArrayFunc(messArray);
